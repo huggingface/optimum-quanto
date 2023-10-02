fix(tensor): rescale was broken after gradient changes

quanto/quantization/calibrate.py
@@ -28,8 +28,8 @@ def calibrate_input(module: torch.nn.Module, input):
             module.in_scale = momentum * module.in_scale + input_scale * (1.0 - momentum)
             if module.bias is not None:
                 bias_scale = module.in_scale * module.weight._scale
-                # (Re)quantize bias
-                module.bias = module.bias.rescale(bias_scale, torch.int32)
+                # (Re)quantize bias with the updated scale
+                module.bias = module.bias.rescale(torch.int32, bias_scale)
         return input
 
 

quanto/quantization/nn/linear.py
@@ -47,9 +47,9 @@ class QLinear(torch.nn.Linear):
         if isinstance(input, QuantizedTensor):
             if input._data.dtype == torch.int32:
                 # Reduce input bitwidth
-                input = input.rescale(self.in_scale)
+                input = input.rescale(torch.int8, self.in_scale)
             out_int32 = torch.nn.functional.linear(input, self.weight, self.bias)
-            return out_int32.rescale(self.out_scale)
+            return out_int32.rescale(torch.int8, self.out_scale)
         else:
             bias = None if self.bias is None else self.bias.dequantize()
             return torch.nn.functional.linear(input, self.weight.dequantize(), bias)

quanto/quantization/tensor.py
@@ -34,6 +34,31 @@ class Dequantizer(Function):
         return gO
 
 
+class ReQuantizer(Function):
+    @staticmethod
+    def forward(ctx, base, int_dtype=torch.int8, scale=None):
+        dst_iinfo = torch.iinfo(int_dtype)
+        if scale is None:
+            if int_dtype == base._data.dtype:
+                return base
+            # Assuming the base scale is correct, simply project to the target integer range
+            src_iinfo = torch.iinfo(base._data.dtype)
+            int_rescale = dst_iinfo.max / src_iinfo.max
+            scale = base._scale * src_iinfo.max / dst_iinfo.max
+        else:
+            # It is up to the caller to make sure the scale is consistent with the target int dtype
+            int_rescale = base._scale / scale
+        data = torch.clamp(torch.round(base._data * int_rescale), min=dst_iinfo.min, max=dst_iinfo.max).to(int_dtype)
+        # The instantiation of the quantized tensor must happen within the context of the Function
+        # for the autograd magic to work.
+        return QuantizedTensor(data, scale)
+
+    @staticmethod
+    def backward(ctx, gO):
+        # For autograd, requantization is a no-op
+        return gO, None, None
+
+
 def q_to_copy(func, t, dtype=None, **kwargs):
     # Ignore dtype and use the inner data tensors dtypes instead
     out_data = func(t._data, dtype=t._data.dtype, **kwargs)
@@ -164,10 +189,9 @@ class QuantizedTensor(torch.Tensor):
         """Differentiable dequantization function"""
         return Dequantizer.apply(self)
 
-    def rescale(self, scale, int_dtype=torch.int8):
-        # We need to take the existing scale into account
-        rescaling_factor = scale / self._scale
-        return LinearQuantizer.apply(self._data, int_dtype, rescaling_factor)
+    def rescale(self, int_dtype=torch.int8, scale=None):
+        """Differentiable requantization function"""
+        return ReQuantizer.apply(self, int_dtype, scale)
 
     @classmethod
     def __torch_dispatch__(cls, func, types, args, kwargs=None):

test/test_quantized_module.py
@@ -29,7 +29,8 @@ def test_quantize_linear(batch_size, tokens, embeddings, use_bias, device):
     with torch.no_grad():
         int_qout = qlinear(qinputs)
     assert qout._scale == int_qout._scale
-    assert torch.allclose(qout.dequantize(), int_qout._data)
+    # There may be a slight difference, but of at most one quantization interval
+    assert torch.max(torch.abs(qout._data - int_qout._data)) <= 1
 
 
 def test_qlinear_serialization():