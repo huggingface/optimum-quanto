9ed9992d4b17ace05332758bbf6c9dad29870378
David Corvoysier <david@huggingface.co>
2023-10-04 08:11:42 +0000

    feat(calibrate): use float path to calibrate outputs
    Instead of dequantizing inputs in the pre-hook and expecting the forward
    method of the module to operate using the float path (which is required to
    correctly calibrate outputs), the post-hook now explicitly calls the
    parent forward to evaluate the dequantized inputs using the float path.
    The implicit requirement is that each quantized module must inherit from a parent
    float module.

--- quanto/quantization/calibrate.py
+++ quanto/quantization/calibrate.py
@@ -7,7 +7,7 @@ from torch.nn.modules.module import (
 )
 
 from .nn import QLinear
-from .tensor import QuantizedTensor, scale_max
+from .tensor import QuantizedTensor
 
 
 momentum = 0.9
@@ -17,18 +17,21 @@ def calibrate_input(module: torch.nn.Module, input):
     if isinstance(module, QLinear):
         # We want to requantize with the most accurate scale
         input = input[0].dequantize()
-        input_scale = scale_max(input, torch.int8)
+        input = QuantizedTensor.quantize(input, torch.int8)
         if torch.all(module.in_scale == 1):
-            module.in_scale = input_scale
+            module.in_scale = input._scale
         else:
-            module.in_scale = momentum * module.in_scale + input_scale * (1.0 - momentum)
+            module.in_scale = momentum * module.in_scale + input._scale * (1.0 - momentum)
         return input
 
 
-def calibrate_output(module: torch.nn.Module, input: torch.Tensor, output: torch.Tensor):
+def calibrate_output(module: torch.nn.Module, input, output):
     if isinstance(module, QLinear):
-        output = output.dequantize()
-        output = QuantizedTensor.quantize(output)
+        # Reevaluate output using float path
+        input = input[0].dequantize()
+        output = super(module.__class__, module).forward(input)
+        # Requantize output with the most accurate scale
+        output = QuantizedTensor.quantize(output, torch.int8)
         if torch.all(module.out_scale == 1):
             module.out_scale = output._scale
         else:
