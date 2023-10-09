a32b73a9d1fb5bd397f1709f8eaac6d5d4eec5ca
David Corvoysier <david@huggingface.co>
2023-10-09 10:43:45 +0200

    fix(setup): add correct metadata

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name="quanto",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    author_email="david@huggingface.co",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
