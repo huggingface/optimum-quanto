e8f01ac8ea66a9ac8587aa37dd5cafb10051ff94
David Corvoysier <david@huggingface.co>
2023-10-09 10:44:34 +0200

    REVERTME: workaround setuptools bugs
    Package metadata form pyproject.toml is not properly parsed.

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="quanto",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
+    url="https://github.com/huggingface/quanto",
+    author="David Corvoysier",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
