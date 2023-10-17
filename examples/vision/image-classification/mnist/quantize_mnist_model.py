8de2d457727c1c919ad2ec2c1daad99c03c4add4
David Corvoysier <david@huggingface.co>
2023-10-17 10:35:09 +0200

    refactor(examples): create subfolders by tasks

--- examples/vision/image-classification/mnist/quantize_mnist_model.py
+++ examples/vision/image-classification/mnist/quantize_mnist_model.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import time
 from tempfile import TemporaryDirectory
 
 import torch
@@ -17,6 +18,7 @@ def test(model, device, test_loader):
     test_loss = 0
     correct = 0
     with torch.no_grad():
+        start = time.time()
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
@@ -25,12 +27,13 @@ def test(model, device, test_loader):
             test_loss += F.nll_loss(output, target, reduction="sum").item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
+        end = time.time()
 
     test_loss /= len(test_loader.dataset)
 
     print(
-        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
-            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        "\nTest set evaluated in {:.2f} s: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            end - start, test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
         )
     )
 
@@ -82,17 +85,21 @@ def main():
     )
     parser.add_argument("--seed", type=int, default=1, metavar="S", help="random seed (default: 1)")
     parser.add_argument("--model", type=str, default="dacorvo/mnist-mlp", help="The name of the trained Model.")
+    parser.add_argument("--device", type=str, default=None, help="The device to use for evaluation.")
     parser.add_argument("--stats", action="store_true", default=False, help="Display quantization statistics")
     args = parser.parse_args()
 
     torch.manual_seed(args.seed)
 
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-    elif torch.backends.mps.is_available():
-        device = torch.device("mps")
+    if args.device is None:
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+        elif torch.backends.mps.is_available():
+            device = torch.device("mps")
+        else:
+            device = torch.device("cpu")
     else:
-        device = torch.device("cpu")
+        device = torch.device(args.device)
 
     dataset_kwargs = {"batch_size": args.batch_size}
     if torch.cuda.is_available():
