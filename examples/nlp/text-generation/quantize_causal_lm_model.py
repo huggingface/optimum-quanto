f0a40e5d8deb6a0136329e09c58525d876c019fe
David Corvoysier <david@huggingface.co>
2023-10-26 18:09:35 +0200

    fix: actually add example modifications

--- examples/nlp/text-generation/quantize_causal_lm_model.py
+++ examples/nlp/text-generation/quantize_causal_lm_model.py
@@ -5,7 +5,7 @@ from tempfile import TemporaryDirectory
 
 import torch
 from datasets import load_dataset
-from transformers import AutoTokenizer, OPTForCausalLM
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from quanto.quantization import calibration, freeze, quantize
 
@@ -24,16 +24,16 @@ def generate(model, tokenizer, device, prompt):
     )
     end = time.time()
     generated_text = tokenizer.decode(outputs[0])
-    print(f"{generated_text} [{end - start:.2f} s]")
+    print(f"Generated '{generated_text}' in [{end - start:.2f} s]")
 
 
 @torch.no_grad()
-def evaluate_model(model, tokenizer, dataset, device):
+def evaluate_model(model, tokenizer, dataset, device, batch_size):
     model.eval()
     # The task is to predict the last token of the input.
     total, hit = 0, 0
     start = time.time()
-    for batch in dataset.iter(batch_size=32):
+    for batch in dataset.iter(batch_size=batch_size):
         inputs = tokenizer(batch["text"], return_tensors="pt", padding=True)
         input_ids = inputs.input_ids.to(device)
         attention_mask = inputs.attention_mask.to(device)
@@ -55,9 +55,11 @@ def main():
     parser.add_argument(
         "--model",
         type=str,
-        default="facebook/opt-125m",
+        default="EleutherAI/pythia-160m",
         help="The name of the trained Model.",
     )
+    parser.add_argument("--samples", type=int, default=100, help="The number of samples to use for evaluation.")
+    parser.add_argument("--batch_size", type=int, default=32, help="The batch_size for evaluation (and calibration).")
     parser.add_argument("--device", type=str, default=None, help="The device to use for generation.")
     args = parser.parse_args()
 
@@ -73,44 +75,43 @@ def main():
     else:
         device = torch.device(args.device)
 
-    model = OPTForCausalLM.from_pretrained(args.model).to(device)
+    model = AutoModelForCausalLM.from_pretrained(args.model).to(device)
     tokenizer = AutoTokenizer.from_pretrained(args.model)
     tokenizer.pad_token_id = tokenizer.eos_token_id
     tokenizer.padding_side = "left"
-    dataset = load_dataset("lambada", split="validation[:100]")
+    dataset = load_dataset("lambada", split=f"validation[:{args.samples}]").shuffle()
 
     prompt = "One of my fondest memory is"
     print("Float model")
-    # generate(model, tokenizer, device, prompt)
-    evaluate_model(model, tokenizer, dataset, device)
+    generate(model, tokenizer, device, prompt)
+    evaluate_model(model, tokenizer, dataset, device, args.batch_size)
     # Quantize model
     quantize(model)
-    print(model)
     # Test inference
     print("Quantized model")
-    # generate(model, tokenizer, device, prompt)
-    evaluate_model(model, tokenizer, dataset, device)
+    generate(model, tokenizer, device, prompt)
+    evaluate_model(model, tokenizer, dataset, device, args.batch_size)
     # Test inference with calibration
     print("Quantized calibrated model")
     with calibration():
-        evaluate_model(model, tokenizer, dataset, device)
+        evaluate_model(model, tokenizer, dataset, device, args.batch_size)
     # Freeze model
     freeze(model)
     print("Quantized frozen model")
     generate(model, tokenizer, device, prompt)
-    evaluate_model(model, tokenizer, dataset, device)
+    evaluate_model(model, tokenizer, dataset, device, args.batch_size)
     # Now save the model and reload it to verify quantized weights are restored
     with TemporaryDirectory() as tmpdir:
         opt_file = os.path.join(tmpdir, "opt.pt")
         torch.save(model.state_dict(), opt_file)
         # Reinstantiate a model with float weights
-        model_reloaded = OPTForCausalLM.from_pretrained(args.model).to(device)
+        model_reloaded = AutoModelForCausalLM.from_pretrained(args.model).to(device)
         quantize(model_reloaded)
         # When reloading we must assign instead of copying to force quantized tensors assignment
         model_reloaded.load_state_dict(torch.load(opt_file), assign=True)
     print("Quantized model with serialized integer weights")
     generate(model, tokenizer, device, prompt)
-    evaluate_model(model_reloaded, tokenizer, dataset, device)
+    evaluate_model(model, tokenizer, dataset, device, args.batch_size)
 
 
 if __name__ == "__main__":
