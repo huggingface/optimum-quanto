acea6378e9b5d6d0d0d4fa57f8ec5d1522ef620f
David Corvoysier <david@huggingface.co>
2023-11-14 16:12:59 +0100

    feat(calibration): add momentum parameter

--- quanto/quantization/calibrate.py
+++ quanto/quantization/calibrate.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+from functools import partial
 
 import torch
 from torch.nn.modules.module import (
@@ -13,10 +14,7 @@ from .qtensor import QTensor, absmax_scale
 __all__ = ["calibration"]
 
 
-momentum = 0.9
-
-
-def calibrate_input(module: torch.nn.Module, input):
+def calibrate_input(module: torch.nn.Module, input, momentum: float = 0.9):
     if isinstance(module, QModuleMixin):
         # Evaluate the actual scale of the input
         input = input[0]
@@ -35,8 +33,9 @@ def calibrate_input(module: torch.nn.Module, input):
         return input
 
 
-def calibrate_output(module: torch.nn.Module, input, output):
+def calibrate_output(module: torch.nn.Module, input, output, momentum=0.9):
     if isinstance(module, (QModuleMixin)):
+        print(momentum)
         # Reevaluate output using float path and get its actual scale
         input = input[0]
         if isinstance(input, QTensor):
@@ -53,11 +52,11 @@ def calibrate_output(module: torch.nn.Module, input, output):
 
 
 @contextmanager
-def calibration():
+def calibration(momentum=0.9):
     """A context to calibrate quantized modules."""
     try:
-        pre_handle = register_module_forward_pre_hook(calibrate_input)
-        post_handle = register_module_forward_hook(calibrate_output)
+        pre_handle = register_module_forward_pre_hook(partial(calibrate_input, momentum=momentum))
+        post_handle = register_module_forward_hook(partial(calibrate_output, momentum=momentum))
         yield
     finally:
         pre_handle.remove()
