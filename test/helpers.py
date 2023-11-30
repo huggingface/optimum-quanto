91428390dcf42598965bd041c6831983837ee4d4
David Corvoysier <david@huggingface.co>
2023-11-30 10:34:00 +0100

    test(helpers): increase quantization error verbosity

--- test/helpers.py
+++ test/helpers.py
@@ -14,6 +14,9 @@ def random_qtensor(shape, dtype=torch.float32):
 
 def q_assert_close(x: torch.Tensor, xq: QTensor):
     # Absolute error is the quantization scale
-    atol = max(xq._scale, 1e-6)
-    abs_error = torch.max(torch.abs(x - xq.dequantize()))
-    assert abs_error <= atol, f"error {abs_error:.4f} exceeds atol {atol:.4f}"
+    atol = torch.maximum(xq._scale, torch.tensor(1e-6))
+    abs_error = torch.abs(x - xq.dequantize())
+    if not torch.all(abs_error <= atol):
+        print("Float", x)
+        print("Quantized", xq.dequantize())
+        raise ValueError("Error exceeds absolute tolerance")
