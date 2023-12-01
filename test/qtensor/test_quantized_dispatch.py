b32f532d95a8ffa16b3a278e362537b32a6040e7
David Corvoysier <david@huggingface.co>
2023-12-01 10:23:01 +0000

    test(qtensor): fix ids

--- test/qtensor/test_quantized_dispatch.py
+++ test/qtensor/test_quantized_dispatch.py
@@ -80,7 +80,7 @@ def test_dot(input_size, device):
 @pytest.mark.parametrize("batch_size", [1, 10])
 @pytest.mark.parametrize("tokens, embeddings", [(5, 5), (32, 32), (10, 32)])
 @pytest.mark.parametrize("use_bias", [True, False], ids=["bias", "no-bias"])
-@pytest.mark.parametrize("weight_axis", [None, 0], ids=["per-axis", "per-tensor"])
+@pytest.mark.parametrize("weight_axis", [None, 0], ids=["per-tensor", "per-axis"])
 def test_linear(batch_size, tokens, embeddings, use_bias, weight_axis, device):
     qinputs = random_qtensor((batch_size,) + (tokens, embeddings), dtype=torch.float32).to(device)
     qweight = random_qtensor((embeddings, embeddings), dtype=torch.float32, axis=weight_axis).to(device)
