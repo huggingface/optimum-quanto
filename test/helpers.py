07f8bb9339d401db75026888a7ce0bafcc662588
David Corvoysier <david@huggingface.co>
2023-12-04 13:59:48 +0100

    test(qtensor): improve checks

--- test/helpers.py
+++ test/helpers.py
@@ -3,6 +3,14 @@ import torch
 from quanto.quantization import QTensor, absmax_scale
 
 
+def device_eq(a, b):
+    if a.type != b.type:
+        return False
+    a_index = a.index if a.index is not None else 0
+    b_index = b.index if b.index is not None else 0
+    return a_index == b_index
+
+
 def random_tensor(shape, dtype=torch.float32):
     # Return a random tensor between -1. and 1.
     return torch.rand(shape, dtype=dtype) * 2 - 1
