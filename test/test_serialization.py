58a1ec711973a740d61dde129e696ddc11244cc7
David Corvoysier <david@huggingface.co>
2023-12-05 11:28:42 +0100

    test(serialization): improve qlinear serialization test

--- test/test_serialization.py
+++ test/test_serialization.py
@@ -5,6 +5,7 @@ import torch
 from helpers import random_qtensor, random_tensor
 
 from quanto.quantization import QTensor, absmax_scale
+from quanto.quantization.nn import QLinear
 
 
 @pytest.mark.parametrize("input_shape", [(10,), (1, 10), (2, 10), (10, 32, 32)])
@@ -26,35 +27,23 @@ def test_quantized_tensor_serialization(input_shape, int_dtype, dtype, axis):
     assert qinputs_reloaded.axis == qinputs.axis
 
 
-@pytest.mark.parametrize("quantize_before_load", [True, False])
-def test_quantized_module_serialization(device, quantize_before_load):
+@pytest.mark.parametrize("dtype", [torch.float16, torch.float32], ids=["fp16", "fp32"])
+def test_quantized_module_serialization(dtype, device):
     embeddings = 10
-
-    def new_qlinear(embeddings, device):
-        qweight = random_qtensor((embeddings, embeddings), dtype=torch.float32).to(device)
-        qbias = random_qtensor((embeddings,), dtype=torch.float32).to(device)
-        qlinear = torch.nn.Linear(embeddings, embeddings)
-        with torch.no_grad():
-            qlinear.weight = torch.nn.Parameter(qweight)
-            qlinear.bias = torch.nn.Parameter(qbias)
-        return qlinear
-
-    qlinear = new_qlinear(embeddings, device)
+    linear = torch.nn.Linear(embeddings, embeddings).to(dtype).to(device)
+    linear.to(dtype)
+    qlinear = QLinear.from_module(linear)
+    qlinear.freeze()
     b = io.BytesIO()
     torch.save(qlinear.state_dict(), b)
     b.seek(0)
     state_dict = torch.load(b)
-    if quantize_before_load:
-        qlinear_reloaded = new_qlinear(embeddings, device)
-        # Since the weights are already quantized tensors, we can copy instead of assigning
-        assign = False
-    else:
-        qlinear_reloaded = torch.nn.Linear(embeddings, embeddings)
-        # We need to force assignment instead of copy to replace weights by quantized weights
-        assign = True
-    qlinear_reloaded.load_state_dict(state_dict, assign=assign)
+    qlinear_reloaded = QLinear(embeddings, embeddings)
+    # We need to force assignment instead of copy to replace weights by quantized weights
+    qlinear_reloaded.load_state_dict(state_dict, assign=True)
     for attr in ["weight", "bias"]:
         t = getattr(qlinear, attr)
         t_reloaded = getattr(qlinear_reloaded, attr)
         assert torch.equal(t._data, t_reloaded._data)
         assert torch.equal(t._scale, t_reloaded._scale)
+        assert t_reloaded.dtype == dtype
