9efec6cc4eb8f23d78f020827706c33032001b77
David Corvoysier <david@huggingface.co>
2023-12-05 09:21:25 +0100

    test(serialization): extend QTensor tests

--- test/test_serialization.py
+++ test/test_serialization.py
@@ -2,17 +2,28 @@ import io
 
 import pytest
 import torch
-from helpers import random_qtensor
+from helpers import random_qtensor, random_tensor
+
+from quanto.quantization import QTensor, absmax_scale
 
 
-def test_quantized_tensor_serialization():
-    qinputs = random_qtensor((1, 10, 32), dtype=torch.float32)
+@pytest.mark.parametrize("input_shape", [(10,), (1, 10), (2, 10), (10, 32, 32)])
+@pytest.mark.parametrize("int_dtype", [torch.int8], ids=["int8"])
+@pytest.mark.parametrize("dtype", [torch.float16, torch.float32], ids=["fp16", "fp32"])
+@pytest.mark.parametrize("axis", [None, 0, -1], ids=["per-tensor", "first-axis", "last-axis"])
+def test_quantized_tensor_serialization(input_shape, int_dtype, dtype, axis):
+    inputs = random_tensor(input_shape, dtype=dtype)
+    scale = absmax_scale(inputs, int_dtype, axis)
+    qinputs = QTensor.quantize(inputs, int_dtype, scale)
     b = io.BytesIO()
     torch.save(qinputs, b)
     b.seek(0)
     qinputs_reloaded = torch.load(b)
-    assert torch.equal(qinputs._data, qinputs_reloaded._data)
-    assert torch.equal(qinputs._scale, qinputs_reloaded._scale)
+    assert torch.equal(qinputs_reloaded._data, qinputs._data)
+    assert torch.equal(qinputs_reloaded._scale, qinputs._scale)
+    # We cannot test dtype directly, as it is not set correctly by torch.load
+    assert qinputs_reloaded._scale.dtype == dtype
+    assert qinputs_reloaded.axis == qinputs.axis
 
 
 @pytest.mark.parametrize("quantize_before_load", [True, False])
