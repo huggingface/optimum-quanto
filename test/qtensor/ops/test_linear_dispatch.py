d370b96af3a326db60f79c7de4b2eb309867a98b
David Corvoysier <david@huggingface.co>
2023-12-05 17:55:18 +0100

    test(qtensor): test float16 linear

--- test/qtensor/ops/test_linear_dispatch.py
+++ test/qtensor/ops/test_linear_dispatch.py
@@ -8,19 +8,24 @@ from quanto.quantization import QTensor
 @pytest.mark.parametrize("batch_size", [1, 10])
 @pytest.mark.parametrize("tokens, embeddings", [(5, 5), (32, 32), (10, 32)])
 @pytest.mark.parametrize("use_bias", [True, False], ids=["bias", "no-bias"])
+@pytest.mark.parametrize("dtype", [torch.float32, torch.float16], ids=["fp32", "fp16"])
 @pytest.mark.parametrize("weight_axis", [None, 0], ids=["per-tensor", "per-axis"])
-def test_linear(batch_size, tokens, embeddings, use_bias, weight_axis, device):
-    qinputs = random_qtensor((batch_size,) + (tokens, embeddings), dtype=torch.float32).to(device)
-    qweight = random_qtensor((embeddings, embeddings), dtype=torch.float32, axis=weight_axis).to(device)
+def test_linear(batch_size, tokens, embeddings, use_bias, dtype, weight_axis, device):
+    if dtype == torch.float16 and device == torch.device("cpu"):
+        pytest.skip("torch.ops.aten.addmm is not supported for float16 on CPU.")
+    qinputs = random_qtensor((batch_size,) + (tokens, embeddings), dtype=dtype).to(device)
+    qweight = random_qtensor((embeddings, embeddings), dtype=dtype, axis=weight_axis).to(device)
     if use_bias:
-        bias = random_tensor((embeddings,), dtype=torch.float32).to(device)
-        # Bias must be quantized to int32 with the same scale as the product of the two int8
+        bias = random_tensor((embeddings,), dtype=dtype).to(device)
+        # Bias must be quantized to int16 with the same scale as the product of the two int8
         prod_scale = torch.squeeze(qinputs._scale * qweight._scale)
-        qbias = QTensor.quantize(bias, torch.int32, prod_scale)
+        qbias = QTensor.quantize(bias, torch.int16, prod_scale)
     else:
         qbias = None
     out = torch.nn.functional.linear(
         qinputs.dequantize(), qweight.dequantize(), qbias.dequantize() if use_bias else None
     )
     qout = torch.nn.functional.linear(qinputs, qweight, qbias)
-    q_assert_close(out, qout)
+    # We need to increase rtol for float16
+    rtol = {torch.float32: 1e-5, torch.float16: 1e-2}[dtype]
+    q_assert_close(out, qout, rtol)
