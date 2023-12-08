c6aa3b450a9ff1831d03a248eb418bec4ceb16e7
David Corvoysier <david@huggingface.co>
2023-12-08 09:55:17 +0000

    test(qtensor): isolate mm dispatch tests

--- test/qtensor/ops/test_quantized_dispatch.py
+++ test/qtensor/ops/test_quantized_dispatch.py
@@ -47,26 +47,6 @@ def test_mul_scalar(input_shape, scalar, device):
     q_assert_close(prod, qprod)
 
 
-@pytest.mark.parametrize("input_shape", [(10, 10), (32, 32)])
-def test_matmul(input_shape, device):
-    qa = random_qtensor(input_shape, dtype=torch.float32).to(device)
-    qb = random_qtensor(input_shape, dtype=torch.float32).to(device)
-    qmatmul = torch.matmul(qa, qb)
-    # The outputs should be almost identical if we use the dequantized inputs
-    matmul = torch.matmul(qa.dequantize(), qb.dequantize())
-    q_assert_close(matmul, qmatmul)
-
-
-@pytest.mark.parametrize("input_shape", [(1, 32, 32)])
-def test_bmm(input_shape, device):
-    qa = random_qtensor(input_shape, dtype=torch.float32).to(device)
-    qb = random_qtensor(input_shape, dtype=torch.float32).to(device)
-    qbmm = torch.bmm(qa, qb)
-    # The outputs should be almost identical if we use the dequantized inputs
-    bmm = torch.bmm(qa.dequantize(), qb.dequantize())
-    q_assert_close(bmm, qbmm)
-
-
 @pytest.mark.parametrize("input_size", [1, 10, 32])
 def test_dot(input_size, device):
     qa = random_qtensor((input_size,), dtype=torch.float32).to(device)
