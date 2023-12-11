6e2bbb4ea7c870e92113394b0e06eb8f4fadd2ed
David Corvoysier <david@huggingface.co>
2023-12-11 15:04:18 +0000

    test(qtensor): test absmax with float8

--- test/qtensor/test_absmax.py
+++ test/qtensor/test_absmax.py
@@ -6,10 +6,14 @@ from quanto.quantization import absmax_scale
 
 
 @pytest.mark.parametrize("input_shape", [(10,), (1, 10), (2, 10), (10, 32, 32)])
-@pytest.mark.parametrize("itype", [torch.int8], ids=["int8"])
+@pytest.mark.parametrize(
+    "itype", [torch.int8, torch.float8_e5m2, torch.float8_e4m3fn], ids=["int8", "float8_e5m2", "float8_e4m3"]
+)
 @pytest.mark.parametrize("dtype", [torch.float16, torch.float32], ids=["fp16", "fp32"])
 @pytest.mark.parametrize("axis", [None, 0, -1], ids=["per-tensor", "first-axis", "last-axis"])
 def test_quantize_scale(input_shape, axis, dtype, itype, device):
+    if device.type == "mps" and itype.is_floating_point:
+        pytest.skip("Float8 are not supported on MPS device")
     a = random_tensor(input_shape, dtype=dtype).to(device)
     scale = absmax_scale(a, itype, axis)
     assert scale.dtype == dtype
