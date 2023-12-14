0d98ecba5a8a1c6447a3f6314276a36527316c79
David Corvoysier <david@huggingface.co>
2023-12-14 10:24:23 +0100

    test: add skip_device marker

--- test/conftest.py
+++ test/conftest.py
@@ -12,3 +12,18 @@ elif torch.backends.mps.is_available():
 @pytest.fixture(scope="module", params=devices)
 def device(request):
     return torch.device(request.param)
+
+
+def pytest_configure(config):
+    # register additional markers
+    config.addinivalue_line("markers", "skip_device(type): mark test to be skipped for the specified device type")
+
+
+def pytest_runtest_call(item):
+    fixture_name = "device"
+    if fixture_name in item.fixturenames:
+        # TODO: should be able to recover the fixture id instead of the actual value
+        fixture_arg = item.funcargs[fixture_name].type
+        skip_marks = {mark.args[0] for mark in item.iter_markers(name=f"skip_{fixture_name}")}
+        if fixture_arg in skip_marks:
+            pytest.skip(f"Test skipped for {fixture_name} {fixture_arg}")
