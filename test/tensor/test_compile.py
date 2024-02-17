d534a8cdb35760a0ce4feea81c787a228a422f8f
David Corvoysier <david.corvoysier@gmail.com>
2024-02-17 12:22:25 +0100

    fix(tensor): add missing qtype for dynamo
    Most of the tests are still failing when QTensor.quantize is in the graph.

--- test/tensor/test_compile.py
+++ test/tensor/test_compile.py
@@ -2,7 +2,7 @@ import pytest
 import torch
 from helpers import random_tensor, torch_min_version
 
-from quanto import QTensor, absmax_scale
+from quanto import QTensor, absmax_scale, qint8
 
 
 def compile_for_device(f, device):
@@ -13,32 +13,30 @@ def compile_for_device(f, device):
     return torch.compile(f, backend=backend)
 
 
-@pytest.mark.skip("Fails with missing numpy requirement.")
-@torch_min_version("2.2.0")
+@torch_min_version("2.3.0")
 @pytest.mark.parametrize("input_shape", [(2, 10), (10, 32, 32)])
-@pytest.mark.parametrize("itype", [torch.int8], ids=["int8"])
+@pytest.mark.parametrize("qtype", [qint8], ids=["qint8"])
 @pytest.mark.parametrize("axis", [None, 0, -1], ids=["per-tensor", "first-axis", "last-axis"])
 @pytest.mark.parametrize("dtype", [torch.float32, torch.float16, torch.bfloat16], ids=["fp32", "fp16", "bf16"])
-def test_compile_quantize_tensor(input_shape, itype, axis, dtype, device):
+def test_compile_quantize_tensor(input_shape, qtype, axis, dtype, device):
     if device == torch.device("mps") and dtype == torch.bfloat16:
         pytest.skip("BFloat16 is not supported on MPS")
     a = random_tensor(input_shape, dtype=dtype).to(device)
 
-    def f(x, itype, axis):
-        scale = None if axis is None else absmax_scale(a, itype, axis).to(device)
-        return QTensor.quantize(x, itype, scale)
+    def f(x, qtype, axis):
+        scale = None if axis is None else absmax_scale(a, qtype, axis).to(device)
+        return QTensor.quantize(x, qtype, scale)
 
     compiled_f = compile_for_device(f, device)
-    qa = compiled_f(a, itype, axis)
+    qa = compiled_f(a, qtype, axis)
     assert isinstance(qa, QTensor)
-    assert qa.itype == itype
+    assert qa.qtype == qtype
     assert qa._scale.dtype == dtype
     expected_axis = a.ndim - 1 if axis == -1 else axis
     assert qa.axis == expected_axis
 
 
-@pytest.mark.skip("Fails with missing numpy requirement.")
-@torch_min_version("2.2.0")
+@torch_min_version("2.3.0")
 @pytest.mark.parametrize("qtensor_input", [True, False], ids=["qtensor-input", "tensor-input"])
 def test_compile_qtensor_to(qtensor_input, device):
     input_shape = (10, 32, 32)
@@ -54,5 +52,5 @@ def test_compile_qtensor_to(qtensor_input, device):
         a = QTensor.quantize(a)
     qa = compiled_f(a, torch.float16)
     assert isinstance(qa, QTensor)
-    assert qa.itype == torch.int8
+    assert qa.qtype == qint8
     assert qa._scale.dtype == torch.float16
