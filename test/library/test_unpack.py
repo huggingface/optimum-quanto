09d360f1bee9ca23e26e11d9db112fdbfbdf3390
David Corvoysier <david.corvoysier@gmail.com>
2024-02-18 12:08:22 +0100

    feat(tensor): introduce PackedTensor

--- test/library/test_unpack.py
+++ test/library/test_unpack.py
@@ -4,7 +4,7 @@ import pytest
 import torch
 
 from quanto.library import disable_extensions
-from quanto.tensor.core import pack_weights, qint2, qint4
+from quanto.tensor.packed import pack_weights
 
 
 @pytest.mark.parametrize("bits", [2, 4], ids=["int2", "int4"])
@@ -13,8 +13,7 @@ from quanto.tensor.core import pack_weights, qint2, qint4
 def test_unpack(bits, shape, use_ext, device):
     qmax = 2**bits
     a = torch.randint(0, qmax, shape, dtype=torch.uint8).to(device)
-    bitsdtype = qint2 if bits == 2 else qint4
-    packed_a = pack_weights(a, bitsdtype)
+    packed_a = pack_weights(a, bits)
     context = nullcontext() if use_ext else disable_extensions()
     with context:
         unpacked_a = torch.ops.quanto.unpack(packed_a, bits)
---
+++ test/tensor/test_packed_tensor.py
@@ -0,0 +1,39 @@
+import io
+
+import pytest
+import torch
+from helpers import device_eq
+
+from quanto import PackedTensor
+
+
+@pytest.mark.parametrize("shape", [(10,), (12,), (10, 10), (12, 10), (32, 32)])
+@pytest.mark.parametrize("bits", [2, 4], ids=["int2", "int4"])
+def test_pack_tensor(shape, bits, device):
+    """This test verifies that an integer tensor in the correct range is preserved."""
+    qmax = 2**bits
+    t = torch.randint(0, qmax, shape, dtype=torch.uint8).to(device)
+    packed = PackedTensor.pack(t, bits=bits)
+
+    assert isinstance(packed, PackedTensor)
+    assert packed.dtype == torch.uint8
+    assert device_eq(packed.device, device)
+    assert torch.equal(t, packed.unpack())
+
+
+@pytest.mark.parametrize("bits", [2, 4], ids=["int2", "int4"])
+def test_packed_tensor_serialization(bits, device):
+    qmax = 2**bits
+    shape = (10, 32)
+    t = torch.randint(0, qmax, shape, dtype=torch.uint8).to(device)
+    packed = PackedTensor.pack(t, bits=bits)
+    b = io.BytesIO()
+    torch.save(packed, b)
+    b.seek(0)
+    packed_reloaded = torch.load(b)
+    assert isinstance(packed_reloaded, PackedTensor)
+    assert packed_reloaded.shape == packed.shape
+    assert packed_reloaded.dtype == packed.dtype
+    assert packed_reloaded.bits == packed.bits
+    assert torch.equal(packed_reloaded._data, packed._data)
+    assert torch.equal(t, packed_reloaded.unpack())
