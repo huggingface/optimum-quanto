cb386af4a8c1298c34db083c239a7aa26cf777d7
David Corvoysier <david@huggingface.co>
2024-02-21 13:09:33 +0000

    feat(quanto): remove qint16 and qint32

--- quanto/tensor/qtype.py
+++ quanto/tensor/qtype.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 import torch
 
 
-__all__ = ["qtype", "qint2", "qint4", "qint8", "qint16", "qint32", "qfloat8", "qfloat8_e4m3fn", "qfloat8_e5m2"]
+__all__ = ["qtype", "qint2", "qint4", "qint8", "qfloat8", "qfloat8_e4m3fn", "qfloat8_e5m2"]
 
 
 @dataclass
@@ -26,8 +26,6 @@ class qtype:
 qint2 = qtype("qint2", is_floating_point=False, bits=2, dtype=torch.int8)
 qint4 = qtype("qint4", is_floating_point=False, bits=4, dtype=torch.int8)
 qint8 = qtype("qint8", is_floating_point=False, bits=8, dtype=torch.int8)
-qint16 = qtype("qint16", is_floating_point=False, bits=16, dtype=torch.int16)
-qint32 = qtype("qint32", is_floating_point=False, bits=32, dtype=torch.int32)
 # Alias the float8 representation that has the better support and inference efficiency
 qfloat8 = qtype("qfloat8", is_floating_point=True, bits=8, dtype=torch.float8_e4m3fn)
 qfloat8_e4m3fn = qtype("qfloat8_e4m3fn", is_floating_point=True, bits=8, dtype=torch.float8_e4m3fn)
