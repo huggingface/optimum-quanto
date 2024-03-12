65cec1b6c500ba14e4546092f118975bd03f1f12
Marc Sun <marc@huggingface.co>
2024-03-12 21:35:07 +0100

    feat(udqmm): fix issue with unpacking with transposed tensor

--- quanto/library/ext/cpp/udqmm.cpp
+++ quanto/library/ext/cpp/udqmm.cpp
@@ -1,5 +1,6 @@
 #include "udqmm.h"
 #include "unpack.h"
+#include "ungroup.h"
 
 #include <iostream>
 #include <torch/extension.h>
@@ -8,20 +9,17 @@ using namespace std;
 
 torch::Tensor udqmm(torch::Tensor &input, torch::Tensor &weights, torch::Tensor &scale, torch::Tensor &zeropoint, int axis, int bits, torch::IntArrayRef orig_shape, torch::IntArrayRef unpacked_shape) {
     TORCH_CHECK(zeropoint.scalar_type() == torch::kInt8, "zeropoint must have scalar type: torch.int8");
+    // transpose the weights
+    weights = weights.transpose(0, 1);
     torch::Tensor unpacked_weights = unpack(weights, bits);
-    // slice along the first dim from index 0 to unpacked_shape[0]
-    unpacked_weights = unpacked_weights.slice(0, 0, unpacked_shape[0]);
+    // slice along the first dim from index 0 to unpacked_shape[1] since we transposed the weights
+    unpacked_weights = unpacked_weights.slice(0, 0, unpacked_shape[1]);
+    // transpose back since we finished unpacking the weights
+    unpacked_weights = unpacked_weights.transpose(0, 1);
+
     torch::Tensor dq_output = (unpacked_weights.to(torch::kInt8) - zeropoint).to(scale.dtype()) * scale;
 
-    torch::Tensor ungrouped_output;
-    // Ungroup TODO : put on its own function
-    if (dq_output.sizes() == orig_shape){
-        ungrouped_output = dq_output;
-    }
-    if (axis == 0) {
-        ungrouped_output = torch::reshape(dq_output, orig_shape);
-    }
-    // Finish axis = 1 case
+    torch::Tensor ungrouped_output = ungroup(dq_output, axis, orig_shape);
 
     return torch::mm(input, ungrouped_output);
 }
--- quanto/library/python/udqmm.py
+++ quanto/library/python/udqmm.py
@@ -1,7 +1,5 @@
 import torch
 
-from quanto.tensor.core import ungroup
-
 
 @torch.library.impl("quanto_py::udqmm", "default")
 def udqmm(
@@ -13,11 +11,17 @@ def udqmm(
     bits: int,
     orig_shape: torch.Size,
     unpacked_shape: torch.Size,
-):
+) -> torch.Tensor:
+    # we transpose it back, so it is simpler to unpack since we have the pack + transposed weights
+    weights = weights.transpose(0, 1)
     unpacked_weights = torch.ops.quanto.unpack(weights, bits)
     # TODO : we should proably add that in unpack with with arg unpacked_shape.
-    unpacked_weights_resized = unpacked_weights[: unpacked_shape[0]]
-    shifted_weights = unpacked_weights_resized.to(torch.int8) - zeropoint.to(torch.int8)
+    # Depends if the weights have been transposed or not
+    # if not transposed, we need to do unpacked_weights[: unpacked_shape[0]]
+    unpacked_weights_resized = unpacked_weights[: unpacked_shape[1]]
+    # transpose back
+    unpacked_weights_resized = unpacked_weights_resized.transpose(0, 1)
+    shifted_weights = unpacked_weights.to(torch.int8) - zeropoint
     scaled_weights = shifted_weights.to(scale.dtype) * scale
-    ungrouped_weights = ungroup(scaled_weights, axis, orig_shape)
+    ungrouped_weights = torch.ops.quanto.ungroup(scaled_weights, axis, orig_shape)
     return torch.ops.aten.mm(input, ungrouped_weights)
