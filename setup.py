c8c0a45fefd6120165f77fa14d88fd6fa3f6b471
David Corvoysier <david@huggingface.co>
2024-05-30 16:21:29 +0200

    refactor(package): move README to pyproject

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import find_packages, setup
 
 setup(
     name="quanto",
-    long_description=open("README.md", "r", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
     url="https://github.com/huggingface/quanto",
     author="David Corvoysier",
     packages=find_packages(),
