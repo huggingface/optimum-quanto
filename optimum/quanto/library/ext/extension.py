bbc0fcce37c841163ef241b6823dac9ef65e7048
David Corvoysier <david@huggingface.co>
2024-06-28 15:56:04 +0200

    fix(extensions): rebuild extensions when pytorch is updated

--- optimum/quanto/library/ext/extension.py
+++ optimum/quanto/library/ext/extension.py
@@ -1,6 +1,8 @@
 import os
+import shutil
 from typing import List
 
+import torch
 from torch.utils.cpp_extension import load
 
 
@@ -25,6 +27,13 @@ class Extension(object):
     def lib(self):
         if self._lib is None:
             # We only load the extension when the lib is required
+            version_file = os.path.join(self.build_directory, "pytorch_version.txt")
+            if os.path.exists(version_file):
+                # The extension has already been built: check the torch version for which it was built
+                with open(version_file, "r") as f:
+                    pytorch_build_version = f.read().rstrip()
+                    if pytorch_build_version != torch.__version__:
+                        shutil.rmtree(self.build_directory)
             os.makedirs(self.build_directory, exist_ok=True)
             self._lib = load(
                 name=self.name,
@@ -33,4 +42,7 @@ class Extension(object):
                 extra_cuda_cflags=self.extra_cuda_cflags,
                 build_directory=self.build_directory,
             )
+            if not os.path.exists(version_file):
+                with open(version_file, "w") as f:
+                    f.write(torch.__version__)
         return self._lib
