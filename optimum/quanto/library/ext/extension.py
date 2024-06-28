0eca6c17a53f5b3cc9fcb52e3af38b413dbc1f96
David Corvoysier <david@huggingface.co>
2024-06-28 14:44:25 +0200

    feat(extensions): build extensions in package directory
    This will allow to detect if the extension needs to be rebuilt.

--- optimum/quanto/library/ext/extension.py
+++ optimum/quanto/library/ext/extension.py
@@ -1,3 +1,4 @@
+import os
 from typing import List
 
 from torch.utils.cpp_extension import load
@@ -17,16 +18,19 @@ class Extension(object):
         self.sources = [f"{root_dir}/{source}" for source in sources]
         self.extra_cflags = extra_cflags
         self.extra_cuda_cflags = extra_cuda_cflags
+        self.build_directory = os.path.join(root_dir, "build")
         self._lib = None
 
     @property
     def lib(self):
         if self._lib is None:
             # We only load the extension when the lib is required
+            os.makedirs(self.build_directory, exist_ok=True)
             self._lib = load(
                 name=self.name,
                 sources=self.sources,
                 extra_cflags=self.extra_cflags,
                 extra_cuda_cflags=self.extra_cuda_cflags,
+                build_directory=self.build_directory,
             )
         return self._lib
