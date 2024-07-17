acccf6beed05941dcebda73824236ec0c0cc3398
David Corvoysier <david@huggingface.co>
2024-07-17 11:50:23 +0000

    test(qbits): test TinyGemm tensors only if CUDA arch >= sm80
    The QBitsTensor.create factory method checks for CUDA version, but
    the unit tests that bypass that method must check themselves.

--- test/tensor/qbits/test_tinygemm_packed_tensor.py
+++ test/tensor/qbits/test_tinygemm_packed_tensor.py
@@ -26,6 +26,8 @@ from optimum.quanto import TinyGemmPackedTensor
 @pytest.mark.parametrize("out_features", [128, 256, 512, 1024])
 @pytest.mark.parametrize("random", [True, False])
 def test_pack_tinygemm_tensor(in_features, out_features, random, device):
+    if device.type == "cuda" and torch.cuda.get_device_capability()[0] < 8:
+        pytest.skip(reason="CUDA device >= sm80 not available")
     bits = 4
     qmax = 2**bits
     shape = (out_features, in_features)
@@ -43,6 +45,8 @@ def test_pack_tinygemm_tensor(in_features, out_features, random, device):
 
 @pytest.mark.skip_device("mps")  # Only available with pytorch 2.4
 def test_move_tinygemm_packed_tensor(device):
+    if device.type == "cuda" and torch.cuda.get_device_capability()[0] < 8:
+        pytest.skip(reason="CUDA device >= sm80 not available")
     shape = (256, 256)
     bits = 4
     qmax = 2**bits
