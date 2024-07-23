fc608b82e17aa7a84f30bdafae47fa5c4ccec322
David Corvoysier <david@huggingface.co>
2024-07-23 11:37:32 +0200

    test(library): add a test to verify extensions compilation

---
+++ test/library/test_extensions.py
@@ -0,0 +1,17 @@
+import pytest
+import torch
+
+from optimum.quanto.library import extensions
+
+
+extension_types = ["cpp"]
+if torch.cuda.is_available():
+    extension_types.append("cuda")
+if torch.backends.mps.is_available():
+    extension_types.append("mps")
+
+
+@pytest.mark.parametrize("extension_type", extension_types)
+def test_extension_compilation(extension_type):
+    extension = getattr(extensions, extension_type).ext
+    assert extension.lib is not None
