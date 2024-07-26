601dc193ce0ed381c479fde54a81ba546bdf64d1
David Corvoysier <david@huggingface.co>
2024-07-26 15:11:33 +0000

    chore: bump dev version

--- optimum/quanto/__init__.py
+++ optimum/quanto/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.2.4dev"
+__version__ = "0.2.5dev"
 
 from .calibrate import *
 from .library import *
