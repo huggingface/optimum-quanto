6ac03f157acd83f55b7e8fecaef6bbb1e875d697
David Corvoysier <david@huggingface.co>
2024-07-26 08:23:31 +0000

    refactor(qtensor): dispatch functions per subclass

--- optimum/quanto/tensor/qbytes.py
+++ optimum/quanto/tensor/qbytes.py
@@ -119,6 +119,31 @@ class QBytesTensor(QTensor):
         return QBytesTensor(qtype, axis, size, stride, data, scale)
 
     @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        """Dispatch torch functions applied on this subtensor
+
+        This method is called whenever a torch function (such as `torch.nn.functional.linear`)
+        is called with at least one parameter coresponding to this subtensor:
+
+        - if a quantized implementation exists for the selected function, it is called,
+        - otherwise, the original implementation is called, deactivating further functional dispatch.
+
+        During the execution of the standard torch function, a second-level of dispatch will
+        happen, but this time directly on individual torch Tensor operations (mainly ATEN).
+        """
+        from .qtensor_func import get_qtensor_func
+
+        kwargs = kwargs or {}
+
+        # Look for a func accepting QTensor inputs
+        qfunc = get_qtensor_func(func)
+        if qfunc is not None:
+            return qfunc(*args, **kwargs)
+        # Defer to dispatcher to look instead for QTensor subclasses operations
+        with torch._C.DisableTorchFunctionSubclass():
+            return func(*args, **kwargs)
+
+    @classmethod
     def __torch_dispatch__(cls, op, types, args, kwargs=None):
         from .qbytes_ops import get_qbytestensor_op_dispatch
 
