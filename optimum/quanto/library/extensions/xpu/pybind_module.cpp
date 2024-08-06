8b2b44945fe2560e7468295da54c190905dd8c38
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2024-08-05 22:06:05 -0700

    feat: implement unpack sycl kernels with xpu extension
    This commit implements xpu extension with unpack kernels written
    in sycl. Pytorch supports XPU extensions on Linux starting from
    version 2.7.
    
    $ python bench/kernels/benchmark.py --it 1000
    unpack_2bit[xpu]: python = 0.177 ms, ext = 0.033 ms, ratio = 5.4x
    unpack_4bit[xpu]: python = 0.085 ms, ext = 0.026 ms, ratio = 3.3x
    
    note: without extension ratio is 0.8x.
    
    Requires: https://github.com/pytorch/pytorch/pull/132945
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

---
+++ optimum/quanto/library/extensions/xpu/pybind_module.cpp
@@ -0,0 +1,27 @@
+// Copyright 2024 The HuggingFace Team. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <torch/extension.h>
+#include "unpack.h"
+
+// !IMPORTANT! Some python objects such as dtype, device, are not mapped to C++ types,
+// and need to be explicitly converted using dedicated helpers before calling a C++ method.
+// As a consequence, when an operation takes such an object as parameter, instead
+// of creating a binding directly to the C++ method, you must create a binding to a
+// lambda method that converts the unmapped types and calls the C++ method.
+// See the binding of quantize_symmetric for instance.
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("unpack", &unpack, "unpack");
+}
