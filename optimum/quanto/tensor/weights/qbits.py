eec0c0eaf7a2986fa395646e35535885d4a6426e
jiqing-feng <jiqing.feng@intel.com>
2025-10-10 12:55:19 +0000

    fix tinygemm check
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- optimum/quanto/tensor/weights/qbits.py
+++ optimum/quanto/tensor/weights/qbits.py
@@ -116,7 +116,14 @@ class WeightQBitsTensor(QBitsTensor):
             if type(data) is PackedTensor:
                 data = data.unpack()
             return AWQWeightQBitsTensor(qtype, axis, group_size, size, stride, data, scale, shift, requires_grad)
-        if qtype == qint4 and scale.dtype == torch.bfloat16 and axis == 0 and group_size == 128 and len(size) == 2:
+        if (
+            qtype == qint4
+            and scale.dtype == torch.bfloat16
+            and shift.dtype == torch.bfloat16
+            and axis == 0
+            and group_size == 128
+            and len(size) == 2
+        ):
             if data.device.type == "cpu" or (
                 (data.device.type == "cuda" and torch.version.cuda)
                 and version.parse(torch.version.cuda).release >= (12, 1)
