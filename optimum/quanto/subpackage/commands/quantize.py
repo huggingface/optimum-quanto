033796028ffb8a937ec9457e406e0254282a1ccc
Marc Sun <marc@huggingface.co>
2025-10-14 11:27:33 +0000

    Update import from optimum

--- optimum/quanto/subpackage/commands/quantize.py
+++ optimum/quanto/subpackage/commands/quantize.py
@@ -17,7 +17,7 @@ from typing import TYPE_CHECKING
 
 import torch
 
-from optimum.commands import BaseOptimumCLICommand
+from optimum.commands.base import BaseOptimumCLICommand
 from optimum.exporters import TasksManager
 
 from ...models import QuantizedTransformersModel
